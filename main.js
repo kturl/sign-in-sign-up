b19631d1fedd55980fb83cf5e2bbe1febee81234
Kev <kturley@gmail.com>
2021-01-21 01:11:15 -0500

    Got the basic slider working

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const leftOverlay = document.getElementById('leftOverlay');
+const rightOverlay = document.getElementById('rightOverlay');
+const leftUnderlay = document.getElementById('leftUnderlay');
 
+leftOverlay.addEventListener('click', function() {
+  leftOverlay.classList.toggle('slideRight');
+  leftUnderlay.classList.toggle('slideLeft');
+});
+leftUnderlay.addEventListener('click', function() {
+  leftOverlay.classList.toggle('slideRight');
+  leftUnderlay.classList.toggle('slideLeft');
+});
+// rightOverlay.addEventListener('click', function() {
+//   rightOverlay.classList.toggle('slideLeft');
+//   rightUnderlay.classList.toggle('slideRight');
+// });
